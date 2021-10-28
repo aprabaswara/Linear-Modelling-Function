bb0f62abf619e04241b2d0d6a9d4b8184f12ceb5
unknown <aprabaswara@gmail.com>
2021-10-28 01:55:51 +0100

    P3S2264710.R

--- P3S2264710.R
+++ P3S2264710.R
@@ -1,12 +1,32 @@
 ##Aditya Prabaswara Mardjikoen (S2264710)
+library(ggplot2)
+dat <- mpg
+formula <- cty~fl+displ+cyl
 linmod <- function(formula,dat){
-  X <- model.matrix(~group,dat) ##model matrix
+  X <- model.matrix(formula,dat) ##model matrix
   qrx <- qr(X) ##get QR decomposition of model matrix
   y <- model.frame(formula,dat)[[1]] ##response
   yname <- all.vars(formula)[1] ##response variable name
   p <- ncol(X) ##number of parameters
-  V <- cov(dat) ##covariance matrix
+  V <- cov(X) ##covariance matrix
+  
   beta <- backsolve(qr.R(qrx),qr.qty(qrx,y)[1:p]) ##get beta
+  names(beta) <- colnames(X) ##label beta column based on the column name
+  mu <- X %*% beta ##fitted values
+  sigma <- sqrt(sum(mu-y)^2/(nrow(dat)-p-1))##estimated standard deviation
+  
+  flev <- vector(mode = "list")##initialize factor list
+  flev_var <- c()
+  for (i in colnames(dat)){
+    if (is.factor(dat[,i])==TRUE){
+      flev.append(i)
+      flev_var <- c(flev_var, i)
+      }
+    }
+  names(flev)<-flev_var
+  
+  model_summary <- list(beta=beta, mu=mu, y=y,yname=yname,formula=formula,flev=flev, sigma=sigma)
+  return(model_summary)
 }
 
 print.linmod <- function(x){
