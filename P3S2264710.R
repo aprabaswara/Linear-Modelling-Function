02ff755500ef8e6139d06744e33337330fe10cff
unknown <aprabaswara@gmail.com>
2021-11-04 01:26:29 +0000

    P3S2264710.R

--- P3S2264710.R
+++ P3S2264710.R
@@ -32,82 +32,61 @@ linmod <- function(formula,dat){
   ##flev = a named list containing factor and level factor; sigma = the estimated standard deviation of the regression model
   
   
-  ##initialize the QR decomposition calculation
   X <- model.matrix(formula,dat) ##model matrix
   qrx <- qr(X) ##get QR decomposition of model matrix
   
   
-  
-  ##find response variable and the number of parameters
   y <- model.frame(formula,dat)[[1]] ##response variable data
   yname <- all.vars(formula)[1] ##response variable name
   p <- ncol(X) ##number of parameters
   n <- nrow(X) ##number of observations
   
   
-  
-  ##calculate the estimated regression parameter
   beta <- backsolve(qr.R(qrx),qr.qty(qrx,y)[1:p]) ##regression parameter
-  names(beta) <- colnames(X) ##label beta column based on the column name
+  names(beta) <- colnames(X) ##label beta column based on the column name of X
   
   
-  
-  ##calculate fitted values and the estimated standard deviation
-  mu <- X %*% beta ##fitted values
+  mu <- X %*% beta ##fitted values or predicted values
   mu <- drop(mu) ##return fitted value result as a vector
-  sse <- sum((mu-y)^2)##calculate sum squared error
-  sigma <- sqrt(sse/(n-p))##estimated standard deviation
-  
+  sse <- sum((mu-y)^2) ##calculate sum squared error
+  sigma <- sqrt(sse/(n-p)) ##estimated standard deviation
   
   
   ##Recall that in QR decomposition R is an upper triangular matrix. Therefore, R^{T} (transpose of R) is an lower triangular matrix. Thus, we can calculate
   ##R^{-T} by solving R^{T}x=I using forwardsolve and calculate R^{-1}R^{-T} by solving Rx=R^{-T} because R is an upper triangular matrix. 
   
   
-  ##calculate covariance matrix
-  inv_Rt <- forwardsolve(t(qr.R(qrx)),diag(p))##calculate R^{-T}
-  inv_RtR <- backsolve(qr.R(qrx),inv_Rt)##calculate R^{-1}R^{-T}
-  V <- sigma^2*inv_RtR ##covariance matrix
+  inv_Rt <- forwardsolve(t(qr.R(qrx)),diag(p)) ##calculate R^{-T}
+  inv_RtR <- backsolve(qr.R(qrx),inv_Rt) ##calculate R^{-1}R^{-T}
+  V <- sigma^2 * inv_RtR ##covariance matrix
   
   
-  
-  ##rename the column and row of covariance matrix by the regression parameter name
-  rownames(V) <- colnames(X)
-  colnames(V) <- colnames(X)
+  rownames(V) <- colnames(X) ##name the row of covariance matrix by the parameter name
+  colnames(V) <- colnames(X) ##name the column of covariance matrix by the parameter name
   
   
-  
-  ##finding factor variable in the data
-  factors <- names(which(sapply(dat,is.factor)==TRUE))
-  
+  flev <- list() ##initialize factor level list
+  flev_var <- c() ##initialize vector of factor variable name
   
   
-  ##initialize factor level list and vector of factor variable name
-  flev <- list()
-  flev_var <- c()
+  factors <- names(which(sapply(dat,is.factor))) ##finding factor variable in the data
   
+
+  factor_index <- which(all.vars(formula) %in% factors) ##finding factor variable in the regression formula
   
-  
-  ##find level factor and factor variable name
-  factor_index <- which(all.vars(formula)==factors)
-  
+  ##find level factor and the corresponding factor variable name that was used in the formula
   for (i in all.vars(formula)[factor_index]){
-    if (i %in% factors){
       flev <- c(flev,list(levels(dat[,i])))
       flev_var <- c(flev_var, i)
-    }
-  }
+  } 
   
   names(flev) <- flev_var ##label level factor vector with factor variable name
   
-  
-  
-  ##store all required calculation result in a list
+  ##store all required calculation result in a list and return object of class linmod
   model_summary <- list(beta=beta, V=V, mu=mu, y=y,yname=yname,formula=formula, flev=flev, sigma=sigma)
   class(model_summary) <- 'linmod'
   return(model_summary)
 }
-
 
 
 print.linmod <- function(x){
@@ -126,14 +105,14 @@ print.linmod <- function(x){
   se <- sapply(diag(x$V),sqrt) ##standard error of regression parameter
   
   
-  ##displaying the regression model formula along with the parameter and standard error of the parameter
   model_report <- cbind(x$beta,se)##combine beta and standard error into matrix by column
   colnames(model_report) <- c('Estimate','s.e.')##give name to the matrix column
+  
+  ##displaying the regression model formula along with the parameter and standard error of the parameter
   print(x$formula)
   cat('\n')
   print(model_report)
 }
-
 
 
 plot.linmod <- function(x){
@@ -151,7 +130,7 @@ plot.linmod <- function(x){
   plot(x=fitted,y=resid,xlab='Fitted values',ylab='Residuals')##create plot
   lines(lowess(x=fitted,y=resid),col="red")##trend line of residual pattern
   abline(h=0,lty=3)##horizontal line indicating residual equal zero
-  mtext(text="Residuals vs Fitted",side=3)##give title to the plot
+  mtext(text="Residuals vs Fitted",side=3)##plot title
 }
 
 
@@ -163,32 +142,50 @@ predict.linmod <- function(x,newdata){
   
   ##ouput:vector containing the predicted value or response variable by using newdata
   
-  x <- linmod(formula=len~supp+dose,dat=ToothGrowth)
-  newdata1 <- data.frame(supp=c('VC','VC'),dose=c(2.8,1))
-  if (is.null(newdata1[[x$yname]])==TRUE){
-    newdata1[[x$yname]]<-sample(0:1,nrow(newdata1),replace=TRUE)
+  
+  ##recall that newdata is not containing response variable. So, we have to add the dummy
+  ##response variable(0 and 1) so when we specified the formula from x class model matrix
+  ##can detect it in newdata and perform the correct calculation that we desired. 
+  
+  
+  ##give dummy response variable to newdata if this data doesn't contain response variable
+  if (is.null(newdata[[x$yname]])==TRUE){
+    newdata[[x$yname]]<-sample(0:1,nrow(newdata),replace=TRUE)
   }
   
-  for (i in names(x$flev)){
-    if (is.factor(newdata1[[i]])==FALSE){
-      newdata1[[i]] <- factor(newdata1[[i]])
-    }
+  ##convert all variable that supposed to be factor in the newdata if it has factor variable
+  ii <- which(!all.vars(x$formula)[-1] %in% colnames(newdata))
+  if (length(ii)!=0){
+    output <- 'Error, Independent Variable In Formula Not Found In Input Data!'
   }
-  
-  
-  if (colnames(X)==colnames(x$V)){
-    X = model.matrix(x$formula,newdata1)
-    mu_new = X %*% x$beta
+  else if(length(x$flev)==0){
+    output <- 'No Error'
   }
   else{
-    if (length(colnames(X))>length(names(x$beta))){
-      unmatch_index <- which(is.na(match(colnames(X),names(x$beta)))==TRUE)
-      X = model.matrix(x$formula,newdata1)
-      mu_new = X[,-unmatch_index] %*% x$beta
-    }
-    else{
+    for (i in names(x$flev)){
+      if (is.factor(newdata[[i]])==FALSE){
+        newdata[[i]] <- factor(newdata[[i]])
+      }
       
+      jj <- which(!levels(newdata[[i]]) %in% x$flev[[i]])
+      
+      if (length(jj)!=0){
+        output <- 'Error, Level Factor Is Difference!'
+      }else{
+        output <-'No Error'
+      }
     }
   }
   
+  ##display prediction result
+  if (output != 'No Error'){
+    result <- output
+  }
+  else{
+    X_new <- model.matrix(x$formula, data=newdata, xlev=x$flev) ##model matrix for newdata
+    mu_new <- X_new %*% x$beta
+    mu_new <- drop(mu_new)
+    result <- mu_new
+  }
+  return(result)
 }
